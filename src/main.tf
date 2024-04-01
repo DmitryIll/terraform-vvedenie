8a07b331187c6a6cdd644b355ef9b9ace8d7ab7a
DmitryIll <dnillarionov@gmail.com>
2024-04-01 11:42:31 +0300

    1

--- src/main.tf
+++ src/main.tf
@@ -28,7 +28,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx1" {
   image = docker_image.nginx.image_id
-  name  = "example_${random_password.random_string.resulT}"
+  name  = "example_${random_password.random_string.result}"
 
   ports {
     internal = 80
