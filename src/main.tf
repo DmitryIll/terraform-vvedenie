d1941119e7c649968a57dc9092a70aa2183e26cb
DmitryIll <dnillarionov@gmail.com>
2024-04-04 09:03:14 +0300

    3

--- src/main.tf
+++ src/main.tf
@@ -88,10 +88,10 @@ resource "docker_container" "mysql" {
   }
 
   env {
-    "MYSQL_ROOT_PASSWORD=${random_password.ROOT_PASSWORD.result}"
-    "MYSQL_DATABASE=wordpress"
-    "MYSQL_USER=wordpress"
-    "MYSQL_PASSWORD=${random_password.MYSQL_PASSWORD.result}"
-    "MYSQL_ROOT_HOST="%""
+    MYSQL_ROOT_PASSWORD="${random_password.ROOT_PASSWORD.result}"
+    MYSQL_DATABASE="wordpress"
+    MYSQL_USER="wordpress"
+    MYSQL_PASSWORD="${random_password.MYSQL_PASSWORD.result}"
+    MYSQL_ROOT_HOST="%"
   }
 }
