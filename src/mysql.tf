7f4c880b4a1c0637b275dd46cdcd1012c2769e18
DmitryIll <dnillarionov@gmail.com>
2024-04-05 09:05:50 +0300

    3

--- src/mysql.tf
+++ src/mysql.tf
@@ -38,11 +38,12 @@ resource "docker_container" "mysql" {
   image = docker_image.mysql.image_id
   name  = "mysql"
   env = [
-    "MYSQL_ROOT_PASSWORD = ${random_password.ROOT_PASSWORD.result}"
-    "MYSQL_DATABASE = wordpress"
-    "MYSQL_USER = wordpress"
-    "MYSQL_PASSWORD = ${random_password.MYSQL_PASSWORD.result}"
-    "MYSQL_ROOT_HOST = ""%"""]
+    "MYSQL_ROOT_PASSWORD = ${random_password.ROOT_PASSWORD.result}",
+    "MYSQL_DATABASE = wordpress",
+    "MYSQL_USER = wordpress",
+    "MYSQL_PASSWORD = ${random_password.MYSQL_PASSWORD.result}",
+    "MYSQL_ROOT_HOST = ""%"""
+    ]
 
 
   ports {
